19ae1bcba598efec93e44c9069ce94a90f7ae7a2
sijun <sionwer5@gmail.com>
2025-07-16 22:52:07 +0900

    feat: contact 페이지 추가

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import Image from "next/image";
+
+export default function ContactPage() {
+    return (
+        <div className="text-[#333]">
+            {/* CTA 영역 */}
+            <section className="bg-[#fef3c7] py-24 text-center px-4">
+                <h1 className="text-4xl font-bold mb-4">고객상담 및 문의</h1>
+                <p className="text-lg text-[#555]">
+                    원스인테리어는 항상 열려 있습니다.<br />
+                    친절히 상담해드리겠습니다.
+                </p>
+            </section>
+
+            {/* 정보 + 버튼 영역 */}
+            <section className="bg-white py-20 px-4">
+                <div>
+                    {/* 카카오 버튼 */}
+                    <div className="flex justify-center pt-6">
+                        <button
+                            onClick={() =>
+                                window.open("https://open.kakao.com/o/sAfYyKLg", "_blank")
+                            }
+                            className="bg-[#FEE500] text-black px-8 py-4 rounded-full text-lg font-semibold hover:bg-[#F5DC00] transition flex items-center gap-2 hover:cursor-pointer"
+                        >
+                            <Image
+                                width="100"
+                                height="100"
+                                src="/icons/kakaotalk.png"
+                                alt="카카오톡 아이콘"
+                                className="w-6 h-6 object-contain"
+                            />
+                            카카오톡으로 상담하기
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
