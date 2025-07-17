a53a926bf2b4ab8f9089008ecc8bf745b5fb3eab
millwheel <sionwer4@naver.com>
2025-07-17 17:34:37 +0900

    feat: gnb hover 효과 변경

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ export default function Header() {
         <header className="w-full py-4 px-6 shadow-md bg-white sticky top-0 z-50">
             <div className="max-w-6xl mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-bold">ONCE INTERIOR</h1>
-                <nav className="space-x-6">
-                    <Link href="/about" className="hover:underline">회사소개</Link>
-                    <Link href="/house" className="hover:underline">주거공간</Link>
-                    <Link href="/industry" className="hover:underline">상업공간</Link>
-                    <Link href="/contact" className="hover:underline">문의하기</Link>
+                <nav className="text-xl space-x-16 font-bold">
+                    <Link href="/about" className="hover:text-[#a68b5b] transition-colors">회사소개</Link>
+                    <Link href="/house" className="hover:text-[#a68b5b] transition-colors">주거공간</Link>
+                    <Link href="/industry" className="hover:text-[#a68b5b] transition-colors">상업공간</Link>
+                    <Link href="/contact" className="hover:text-[#a68b5b] transition-colors">문의하기</Link>
                 </nav>
             </div>
         </header>
-    )
+    );
 }
