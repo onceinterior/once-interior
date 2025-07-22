f087edc7377e71e67adf877822e59bda4e94f6f4
millwheel <sionwer4@naver.com>
2025-07-22 15:27:10 +0900

    feat: header 이미지 조정

--- src/components/contact-header.tsx
+++ src/components/contact-header.tsx
@@ -1,18 +1,39 @@
 "use client"
+import Image from "next/image";
 import {ChatBubbleLeftRightIcon} from "@heroicons/react/24/solid";
 
-export default function KakaoHeader() {
+export function KakaoHeader() {
     return (
         <div>
             <button
                 onClick={() =>
                     window.open("https://open.kakao.com/me/once_interior", "_blank")
                 }
-                className="bg-[#FEE500] text-black px-4 py-2 rounded-xl text-xl font-semibold hover:bg-[#F5DC00] transition flex items-center gap-2 hover:cursor-pointer"
+                className="bg-[#FEE500] text-black px-2 py-2 text-xl rounded-xl flex items-center gap-2 hover:cursor-pointer"
             >
                 <ChatBubbleLeftRightIcon className="w-5 h-5 text-black" />
-                문의하기
             </button>
         </div>
     )
+}
+
+export function BlogHeader() {
+    return (
+        <div>
+            <button
+                onClick={() =>
+                    window.open("https://blog.naver.com/once_interior", "_blank")
+                }
+                className="bg-[#03C75A] flex items-center gap-2 hover:cursor-pointer"
+            >
+                <Image
+                    src="/icon/naver-blog.png"
+                    alt="블로그 아이콘"
+                    width={36}
+                    height={36}
+                    className="rounded-lg"
+                />
+            </button>
+        </div>
+    );
 }
