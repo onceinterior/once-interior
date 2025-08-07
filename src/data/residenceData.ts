f77fd016d605ed3b84e08c14490974c1fe8e177e
sijun <sionwer5@gmail.com>
2025-08-07 21:54:20 +0900

    feat: 기존 데이터 구조 제거

--- src/data/residenceData.ts
+++ src/data/residenceData.ts
@@ -1,13 +1,8 @@
 import {Post} from "@/data/type";
 
-export const residenceGalleryItems = [
-    { src: "/image/residence/금호2동_28평.jpg", title: "금호2동 28평" },
-    { src: "/image/residence/신동아19동_40평.jpg", title: "신동아19동 40평" },
-    { src: "/image/residence/쌍문_동익4동_24평.jpg", title: "쌍문 동익4동 24평" },
-];
-
 export const residenceItems : Post[] = [
     {
+        id: 1,
         title: "금호2동_28평",
         thumbnailUrl: "/image/residence/금호2동_28평.jpg",
         imagesUrl: [],
